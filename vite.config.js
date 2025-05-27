552774da4aaa57ac79c7f80204b6b8f8f3f66444
poonehmousavi <moosavi.pooneh@gmail.com>
2025-05-27 14:12:24 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dates-website/', // <-- change to your GitHub repo name
+  base: 'poonehmousavi/dates-website/', // <-- change to your GitHub repo name
 })
