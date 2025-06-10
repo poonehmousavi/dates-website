1d15fe513069b1eabd7c2f7e1892ccdbc81f4475
poonehmousavi <moosavi.pooneh@gmail.com>
2025-06-10 00:44:22 -0400

    add dark

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,10 @@ export default function App() {
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
     } else {
       document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
     }
   }, [darkMode]);
 
